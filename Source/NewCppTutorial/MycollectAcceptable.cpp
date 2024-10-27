416637961540bea84ec7b77d25513e4dc862e67b
Yufeng Yang <1299734936@qq.com>
2024-10-27 11:57:20 +0800

    Fifth time : Interface

---
+++ Source/NewCppTutorial/MycollectAcceptable.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MycollectAcceptable.h"
+
+// Add default functionality here for any IMycollectAcceptable functions that are not pure virtual.
